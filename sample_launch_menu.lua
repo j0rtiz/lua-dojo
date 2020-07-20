39c3fa8f0e610d1c3ce3f8a1630ab0849ec95e73
jairo.ortiz <jairo.ortiz@govbr.com.br>
2020-07-20 18:11:37 -0300

    Routines optimization

--- sample_launch_menu.lua
+++ sample_launch_menu.lua
@@ -1,5 +1,6 @@
 local lfs = require "lfs"
 local path = lfs.currentdir()
+local ignored = {["node_modules"] = true, ["src-cordova"] = true, ["dist"] = true}
 local files_path = {}
 local launch_menu = {}
 
@@ -9,7 +10,7 @@ local function files(path)
       local file_path = path .. "\\" .. name
       local mode = lfs.attributes(file_path, "mode")
 
-      if mode == "directory" and name ~= "node_modules" and name ~= "src-cordova" and name ~= "dist" then
+      if mode == "directory" and not ignored[name] then
         files(file_path)
       end
 
@@ -20,27 +21,35 @@ local function files(path)
   end
 end
 
+local function project(file_path)
+  local suffixes = {
+    ["\\APP"] = file_path:gsub("\\(.*)", " [APP]"),
+    ["\\M"] = file_path:gsub("\\(.*)", " [APP]"),
+    ["\\PAINEL"] = file_path:gsub("\\(.*)", " [PAINEL]"),
+    ["\\PAA"] = file_path:gsub("\\(.*)", " [PAINEL]"),
+    ["\\CLIENT"] = file_path:gsub("\\(.*)", " [APP]"),
+    ["\\CORONAVIRUS"] = file_path:gsub("\\(.*)", " [CORONAVIRUS]"),
+    ["\\API"] = file_path:gsub("\\(.*)", " [API]")
+  }
+
+  for suffix in pairs(suffixes) do
+    if file_path:match(suffix) then
+      return suffixes[suffix]
+    end
+  end
+
+  return file_path
+end
+
 local function menu(files_path)
   for _, file_path in pairs(files_path) do
     local file = io.open(file_path, "r")
 
     for line in file:lines() do
       if line:match('"dev"') or line:match('"start:dev"') then
-        local label = file_path:sub(path:len() + 2, file_path:len() - 13):gsub("-", " "):upper()
+        local label = project(file_path:sub(path:len() + 2, file_path:len() - 13):gsub("-", " "):upper())
         local args = {"powershell.exe", "npm run " .. line:match('"([^"]+)": "([^"]+)"')}
         local cwd = file_path:sub(1, file_path:len() - 13)
-
-        if label:match("\\APP") or label:match("\\M") then
-          label = label:gsub("\\(.*)", " [APP]")
-        elseif label:match("\\PAINEL") or label:match("\\PAA") then
-          label = label:gsub("\\(.*)", " [PAINEL]")
-        elseif label:match("\\CLIENT") then
-          label = label:gsub("\\(.*)", " [APP]")
-        elseif label:match("\\CORONAVIRUS") then
-          label = label:gsub("\\(.*)", " [CORONAVIRUS]")
-        elseif label:match("\\API") then
-          label = label:gsub("\\(.*)", " [API]")
-        end
 
         table.insert(launch_menu, {label = label, args = args, cwd = cwd})
       end
